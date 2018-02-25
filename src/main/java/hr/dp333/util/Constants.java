0a18d104622ab7fa52242ed02ae439eb98dbf56c
dominik <dominik@dominik-VirtualBox>
2018-02-25 22:30:01 +0100

    Add constants.

--- src/main/java/hr/dp333/util/Constants.java
+++ src/main/java/hr/dp333/util/Constants.java
@@ -1,5 +1,6 @@
 package hr.dp333.util;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -10,6 +11,8 @@ public class Constants {
 		throw new IllegalStateException("Cannot instantiate class.");
 	}
 
+	public static final String JSON_SOURCE_FILE_NAME = "data.json";
+	public static final String JSON_DATE_FORMAT_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
 	public static final String ACTIVE = "Active";
 	public static final String INACTIVE = "Inactive";
 	public static final List<String> HEADER_COLUMNS = Collections.unmodifiableList(Arrays.asList(
@@ -23,5 +26,7 @@ public class Constants {
 			"No Party - Inactive",
 			"Other - Inactive",
 			"Republican - Inactive"));
+	public static final String HUMAN_READABLE_DATE_FORMAT_PATTERN = "dd. MM. yyyy";
+	public static final DateTimeFormatter HUMAN_READABLE_DATE_FORMATTER = DateTimeFormatter.ofPattern(HUMAN_READABLE_DATE_FORMAT_PATTERN);
 
 }
